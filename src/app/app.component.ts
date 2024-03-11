7cecc9ae645f9184eefbc725d070a028e787a2d9
Allan Santos <allansssoares@gmail.com>
2024-03-11 20:30:44 -0300

    dev: preparando a aplicação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MarvelService } from './services/marvel.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'marvel';
+  isLoggedIn: boolean = false;
+  private subscription!: Subscription;
+
+
+  constructor(private marvelService: MarvelService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.marvelService.isLoggedIn.subscribe((loggedIn) => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { CardCharacterComponent } from './shared/card-character/card-character.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './shared/loading/loading.component';
+import { ErrorToastComponent } from './shared/error-toast/error-toast.component';
+import { CharacterDetailsComponent } from './components/characters/character-details/character-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardCharacterComponent,
+    CharactersComponent,
+    LoadingComponent,
+    ErrorToastComponent,
+    CharacterDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
