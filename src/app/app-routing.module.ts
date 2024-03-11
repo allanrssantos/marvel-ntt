bd039230aae2e9adf6dbffe01733d461874c78da
Allan Santos <allansssoares@gmail.com>
2024-03-11 20:29:39 -0300

    dev: criação das rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CharactersComponent } from './components/characters/characters.component';
+import { AuthGuard } from './guards/auth.guard';
+import { CharacterDetailsComponent } from './components/characters/character-details/character-details.component';
+
+const routes: Routes = [
+  { path: 'characters', component: CharactersComponent, canActivate:[AuthGuard] },
+  { path: 'character', component: CharacterDetailsComponent, canActivate:[AuthGuard] }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
