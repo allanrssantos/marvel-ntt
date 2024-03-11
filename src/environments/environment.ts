f91fd6a15d9cecb92139ee498f7234a16ad5417b
Allan Santos <allansssoares@gmail.com>
2024-03-11 20:36:27 -0300

    dev: apiUrl e Keys.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://gateway.marvel.com/v1/public',
+  publicKey: '4a63e0f2cb39369dc2e104c54723e151',
+  privateKey: '1a207ebc7ce0e6008d00b695e4ecb83853af421e',
 };
